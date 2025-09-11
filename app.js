2be9af97c812725bb3f6ea8db6b50b74cce130a3
ZeyadAhmed-GRZ11 <152599985+ZeyadAhmed-GRZ11@users.noreply.github.com>
2025-09-11 16:30:54 +0300

    detailsData

--- app.js
+++ app.js
@@ -26,35 +26,43 @@ liveReloadServer.server.once("connection", () => {
 // GET Requests
 app.get('/', async (req, res) => {
   const customerData = await Customer.find();
-  res.render("index",{ customerData, currentPage: 'index', successMessage: 'Data submitted successfully!' }) 
+  res.render("index",{ customerData, currentPage: 'index', successMessage: 'Data submitted successfully!', pageTitle: 'Home Page' }) 
 })
+
 
 app.get('/user/add.html', (req, res) => {
-  res.render("user/add", { currentPage: 'add', successMessage: 'Data submitted successfully!' })
-})
-
-app.get('/user/view.html', (req, res) => {
-  res.render("user/view")
-})
+  const successMessage = req.query.success === '1' ? 'Data submitted successfully!' : '';
+  res.render("user/add", { currentPage: 'add', successMessage });
+});
 
 app.get('/user/edit.html', (req, res) => {
   res.render("user/edit")
 })
 
+app.get('/user/:id', async (req, res) => {
+  try {
+    const user = await Customer.findById(req.params.id); // fetch user by ID
+    res.render("user/view", { user, currentPage: 'view', pageTitle: 'View Page' }); // render view with user data
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("User not found");
+  }
+});
+
+
 // POST Requests
-app.post('/user/add.html', (req, res) => {
 
-  console.log(req.body) // to see the form data in the console
-  const customer = new Customer(req.body); // create a new customer document from the form data
-  customer.save() // save the document to the database
-  .then(( ) => {
-    res.redirect('/user/add.html')
-  }) 
-  .catch((err) => { 
-    console.log(err)
-  })
+app.post('/user/add.html', async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.redirect('/user/add.html?success=1');
+  } catch (err) {
+    res.redirect('/user/add.html?success=0');
+  }
+});
 
-})
+
 
 // Database Connection
 mongoose.connect("mongodb+srv://zeyadahmed:0f1WyobCImGVWuzb@swdb.dogreps.mongodb.net/all-data?retryWrites=true&w=majority&appName=SWDB")
