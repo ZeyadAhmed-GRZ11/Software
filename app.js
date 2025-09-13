ef314e4e083297f31dffc0a860ebdb03f4f1430c
ZeyadAhmed-GRZ11 <152599985+ZeyadAhmed-GRZ11@users.noreply.github.com>
2025-09-13 20:46:09 +0300

    some-updates 1.1

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ liveReloadServer.server.once("connection", () => {
   }, 100);
 });
 
+var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+
+
 // GET Requests
 app.get('/', async (req, res) => {
   const customerData = await Customer.find();
@@ -42,13 +46,13 @@ app.get('/', async (req, res) => {
 
 app.get('/user/add.html', (req, res) => {
   const successMessage = req.query.success === '1' ? 'Data submitted successfully!' : '';
-  res.render("user/add", { currentPage: 'add', successMessage });
+  res.render("user/add", { currentPage: 'add', successMessage, country_list: country_list });
 });
 
 app.get('/edit/:id', (req, res) => {
      Customer.findById(req.params.id)
     .then(user => {
-      res.render("user/edit", { user, currentPage: 'edit', pageTitle: 'Edit Page' });
+      res.render("user/edit", { user, currentPage: 'edit', pageTitle: 'Edit Page', country_list: country_list});
     })
     .catch(err => {
       console.log(err);
@@ -66,7 +70,6 @@ app.get('/view/:id', (req, res) => {
       res.status(500).send("User not found");
     });
 });
-
 
 
 // POST Requests
