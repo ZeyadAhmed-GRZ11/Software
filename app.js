7b8777b9848cba4f7a013fae07fe0372862b5c34
ZeyadAhmed-GRZ11 <152599985+ZeyadAhmed-GRZ11@users.noreply.github.com>
2025-09-22 03:24:15 +0300

    Search-Engine

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // to parse form data
 const Customer = require('./models/customerSchema');   //importing schema
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-var moment = require('moment'); // require
-var methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+app.set('view engine', 'ejs') // set ejs as templating engine
+app.use(express.static('public')) // to serve static files
+var moment = require('moment'); // require moment.js for date formatting
+var methodOverride = require('method-override') // to support PUT and DELETE methods in forms
+app.use(methodOverride('_method')) // override with POST having ?_method=DELETE or ?_method=PUT
 
 
 //Auto refresh
@@ -19,6 +19,7 @@ liveReloadServer.watch(path.join(__dirname, 'public'));
  
  
 const connectLivereload = require("connect-livereload");
+const customer = require('./models/customerSchema');
 app.use(connectLivereload());
  
 liveReloadServer.server.once("connection", () => {
@@ -33,7 +34,7 @@ var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguil
 
 // GET Requests
 app.get('/', async (req, res) => {
-  const customerData = await Customer.find();
+  const customerData = await Customer.find(); // fetch all users
   const updateSuccessMessage = req.query.updateSuccess === '1' ? 'Data updated successfully!' : '';
   res.render("index", {
     customerData,
@@ -49,6 +50,10 @@ app.get('/user/add.html', (req, res) => {
   res.render("user/add", { currentPage: 'add', successMessage, country_list: country_list });
 });
 
+// app.get('/user/search', (req, res) => {
+//   res.render("user/search", { customerData : result , currentPage: 'search', pageTitle: 'Search', moment: moment });
+// });
+
 app.get('/edit/:id', (req, res) => {
      Customer.findById(req.params.id)
     .then(user => {
@@ -61,7 +66,7 @@ app.get('/edit/:id', (req, res) => {
 });
 
 app.get('/view/:id', (req, res) => {
-  Customer.findById(req.params.id)
+  Customer.findById(req.params.id) // fetch user by ID
     .then(user => {
       res.render("user/view", { user, currentPage: 'view', pageTitle: 'View Page', moment: moment });
     })
@@ -75,7 +80,7 @@ app.get('/view/:id', (req, res) => {
 // POST Requests
 app.post('/user/add.html', async (req, res) => {
   try {
-    const customer = new Customer(req.body);
+    const customer = new Customer(req.body); // create a new instance of the model
     await customer.save();
     res.redirect('/user/add.html?success=1');
   } catch (err) {
@@ -83,9 +88,37 @@ app.post('/user/add.html', async (req, res) => {
   }
 });
 
+app.post('/search', (req, res) => {
+  console.log("****************************************");
+  const searchText = req.body.searchText.trim();
+  const ageQuery = Number(searchText);
+
+  Customer.find({
+    $or: [
+      { firstName: { $regex: searchText, $options: "i" } },
+      { lastName: { $regex: searchText, $options: "i" } },
+      { age: !isNaN(ageQuery) ? ageQuery : null }
+    ]
+  })
+  .then((result) => {
+    console.log(result);
+    res.render("user/search", {
+      arr: result, 
+      currentPage: 'search',
+      pageTitle: 'Search',
+      moment: moment
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Server Error");
+  });
+});
+
+
 app.post('/edit/:id', async (req, res) => {
   try {
-    await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    await Customer.findByIdAndUpdate(req.params.id, req.body, { new: true }); // update user by ID
     res.redirect('/?updateSuccess=1');
   } catch (err) {
     console.log(err, "Error updating user");
@@ -96,7 +129,7 @@ app.post('/edit/:id', async (req, res) => {
 // delete request
 app.delete('/edit/:id', async (req, res) => {
   try {
-    await Customer.findByIdAndDelete(req.params.id);
+    await Customer.findByIdAndDelete(req.params.id); // delete user by ID
     res.redirect('/');
   } catch (err) {
     res.status(500).send("Error deleting user");
