9630cb176e00898c14d8b42f9db041aeea8e1b05
ZeyadAhmed-GRZ11 <152599985+ZeyadAhmed-GRZ11@users.noreply.github.com>
2025-10-06 21:56:19 +0300

    project-Release1.1

--- routes/addUser.js
+++ routes/addUser.js
@@ -5,23 +5,10 @@ var moment = require('moment'); // require moment.js for date formatting
 const UserController = require('../controllers/UserController');
 
 
-var country_list = ["America","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
-router.get('/add.html', (req, res) => {
-  const successMessage = req.query.success === '1' ? 'Data submitted successfully!' : '';
-  res.render("user/add", { currentPage: 'add', successMessage, country_list: country_list });
-});
+router.get('/add.html',UserController.AddUserPage);
 
-
-router.post('/add.html', async (req, res) => {
-  try {
-    const customer = new Customer(req.body); // create a new instance of the model
-    await customer.save();
-    res.redirect('/user/add.html?success=1');
-  } catch (err) {
-    res.redirect('/user/add.html?success=0');
-  }
-});
+router.post('/add.html',UserController.Post_AddUserPage);
 
 
 module.exports = router;
--- routes/searchEngine.js
+++ routes/searchEngine.js
@@ -4,32 +4,8 @@ const Customer = require('../models/customerSchema');   //importing schema
 var moment = require('moment'); // require moment.js for date formatting
 const UserController = require('../controllers/UserController');
 
-router.post('/search', (req, res) => {
-  console.log("****************************************");
-  const searchText = req.body.searchText.trim();
-  const ageQuery = Number(searchText);
 
-  Customer.find({
-    $or: [
-      { firstName: { $regex: searchText, $options: "i" } },
-      { lastName: { $regex: searchText, $options: "i" } },
-      { age: !isNaN(ageQuery) ? ageQuery : null }
-    ]
-  })
-  .then((result) => {
-    console.log(result);
-    res.render("user/search", {
-      arr: result, 
-      currentPage: 'search',
-      pageTitle: 'Search',
-      moment: moment
-    });
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Server Error");
-  });
-});
+router.post('/search', UserController.SearchEngine); 
 
 
 module.exports = router;
